57d8462bfefb5d1da1d9052aeae9db73216e76e9
ZoeRuiz <zoe_alge89@outlook.es>
2025-06-12 01:31:29 +0200

    landing a medias

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home   from './pages/Home';
 
-function App() {
-
+export default function App() {
   return (
     <>
-     <Header />
+      <Header />
+      <Home />
     </>
-  )
+  );
 }
-
-export default App
